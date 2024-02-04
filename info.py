ee960242e3137bf22ca88f3869ca5266815fe52a
randeepsingh48 <156921899+randeepsingh48@users.noreply.github.com>
2024-02-04 22:03:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -26,8 +26,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://graph.org/file/10666f11431b8869ee866.jpg')).split()#SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/46443096bc6895c74a716.jpg")
+PICS = (environ.get('PICS', 'https://graph.org/file/d3a23883d75ecec39ff23.jpg')).split()#SAMPLE PIC
+NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/d3a23883d75ecec39ff23.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
 PICS_JOIN = (environ.get('PICS_JOIN', 'https://graph.org/file/aafda289da8b859cc23aa.jpg')).split()
