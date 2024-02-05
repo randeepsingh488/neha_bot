25e5d597919d12f865f3e27136aff397996d550f
randeepsingh48 <156921899+randeepsingh48@users.noreply.github.com>
2024-02-05 22:49:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -30,7 +30,7 @@ PICS = (environ.get('PICS', 'https://graph.org/file/d3a23883d75ecec39ff23.jpg'))
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/d3a23883d75ecec39ff23.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/5b421e9982fc8cbb586c6.jpg")
-PICS_JOIN = (environ.get('PICS_JOIN', 'https://graph.org/file/aafda289da8b859cc23aa.jpg')).split()
+PICS_JOIN = (environ.get('PICS_JOIN', 'https://graph.org/file/5b3bbaca0113c6c7dca3a.jpg')).split()
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5311861748').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002013273078').split()]
